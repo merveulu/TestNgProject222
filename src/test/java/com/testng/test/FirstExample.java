7190f84f6336994bc1204ad170e76e54b9a0a1b7
Merve <merve.maths@gmail.com>
2021-04-09 21:40:18 +0200

    Initial commit

---
+++ src/test/java/com/testng/test/FirstExample.java
@@ -0,0 +1,12 @@
+package com.testng.test;
+
+
+import org.testng.annotations.Test;
+
+public class FirstExample {
+
+    @Test
+    public void FirstExample(){
+        Assert.assertTrue(true);
+    }
+}
